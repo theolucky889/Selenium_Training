f56288b4a5d2e10defb61671abc4dda92501cc1a
theolucky889 <theolucky889@gmail.com>
2024-09-08 21:14:42 +0800

    Update test.py

---
+++ Selenium_test_webdriver_manager/test.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://www.rcvacademy.com")
+driver.maximize_window()
+print(driver.title)
