83b1065993d1a7fe384cd2232ad2437f2f34b575
theolucky889 <theolucky889@gmail.com>
2024-10-20 22:05:59 +0800

    Update DemoElementLocators.py

--- Selenium/DemoElementLocators.py
+++ Selenium/DemoElementLocators.py
@@ -2,13 +2,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://myedit.online/en/photo-editor")
-# driver.find_element_by_id('Sign_In_Button').click()
-# driver.find_element_by_id('mui-4').send_keys('myedit_premium_credits@cyberlink.com')
-driver.find_element(by.ID, 'Sign_In_Button').click()
+
+driver.find_element(By.ID, "Sign_In_Button").click()
+driver.find_element(By.ID, "mui-2").send_keys("myedit_premium_credits@cyberlink.com")
+driver.find_element(By.ID, "password")
 time.sleep(10)
         
